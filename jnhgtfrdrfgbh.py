dbab2ed18fc59c5ae69cbc3fed4279fb8d633909
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-23 13:10:30 -0500

    Create jnhgtfrdrfgbh.py

---
+++ jnhgtfrdrfgbh.py
@@ -0,0 +1,90 @@
+import os
+import platform
+import sys
+from scripts.sprint import sprint
+from scripts.colors import ran,y,r,g,c
+import time
+print("\n" * 128)
+print("                                                         ▄▄ ▄████▄▐▄▄▄▌           ")
+print("            ▄▀▄     ▄▀▄                                 ▐  ████▀███▄█▄▌           ")
+print("           ▄█░░▀▀▀▀▀░░█▄                              ▐ ▌  █▀▌  ▐▀▌▀█▀            ")
+print("       ▄▄  █░░░░░░░░░░░█  ▄▄        //+++\\            ▀   ▌ ▌  ▐ ▌               ")
+print("      █▄▄█ █░░▀░░┬░░▀░░█ █▄▄█      // |||  \\              █ █  ▐▌█               ")
+print("██╗░░██╗██╗████████╗████████╗██╗░░░██╗|||████████╗░█████╗░░█████╗░██╗░░░░░░██████╗")
+print("██║░██╔╝██║╚══██╔══╝╚══██╔══╝╚██╗░██╔╝|||╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝")
+print("█████═╝░██║░░░██║░░░░░░██║░░░░╚████╔╝░|||░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░")
+print("██╔═██╗░██║░░░██║░░░░░░██║░░░░░╚██╔╝░░|||░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗")
+print("██║░╚██╗██║░░░██║░░░░░░██║░░░░░░██║░░░|||░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝")
+print("╚═╝░░╚═╝╚═╝░░░╚═╝░░░░░░╚═╝░░░░░░╚═╝░░░|||░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░")
+print("                       \\             |||             //                          ")
+print("                        \\            |||            //                           ")
+print("                         \\           |||           //                            ")
+print("                          \\          |||          //                             ")
+print("                     /===================================\                        ")
+print("                   //                -Fun-                \\                      ")
+print("                  ||                -Hacks-                ||                     ")
+print("                  ||                 -For-                 ||                     ")
+print("                  ||                 -The-                 ||                     ")
+print("                  ||                -Whole-                ||                     ")
+print("                   \\                -Family-             //                      ")
+print("                     \====================================/                       ")
+print(" ")
+print(" ")
+print("Please type 1, 2, 3, or 4")
+print(" ")
+print("[1] How to use")
+print("||| \--------/")
+print(" ")
+print("[2] Information")
+print("||| \---------/")
+print(" ")
+print("[3] Flooder")
+print("||| \-----/")
+print(" ")
+print("[4] Start Hack")
+print("||| \--------/")
+choice = input("")
+
+if choice == "4":
+    print(" ")
+    print("Starting...")
+    time.sleep(1)
+    print("\n" * 64) ## imma this makes more sense
+    from subprocess import call
+    call(["python", "Kahoot/kahoot.py"])
+
+if choice == "1":
+    print(" ")
+    print("Loading...")
+    time.sleep(1)
+    from subprocess import call
+    call(["python", "Kahoot/HTU.py"])
+
+if choice == "2":
+    print(" ")
+    print("Loading...")
+    time.sleep(1)
+    from subprocess import call
+    call(["python", "Kahoot/Info.py"])
+    
+if choice == "3":
+    print(" ")
+    print("Loading...")
+    time.sleep(1)
+    from subprocess import call
+    call(["python", "Kahoot/flood/Info.py"])
+
+
+time.sleep(10)
+print("For more hacks go to↓↓↓")
+print("https://mem.rip/kahoot/")
+print(" ")
+print(" ")
+print("Software Made by CPScript")
+print(" ")
+print("   ▄▀ ▄▀   ")
+print("    ▀  ▀   ")
+print("  █▀▀▀▀▀█▄ ")
+print("  █░░░░░█ █")
+print("  ▀▄▄▄▄▄▀▀ ")
+print(">CPScript<")
